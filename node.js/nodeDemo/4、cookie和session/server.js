607bb7809e8fa1901cbee753f9e721c23d391c72
1246269377 <1246269377@qq.com>
2017-05-11 20:26:45 +0800

    .

--- node.js/nodeDemo/4、cookie和session/server.js
+++ node.js/nodeDemo/4、cookie和session/server.js
@@ -3,6 +3,7 @@
  */
 const express=require('express');
 const cookieParser=require('cookie-parser');
+const cookieSession=require('cookie-session');
 
 var server=express();
 server.listen(8080);
@@ -12,9 +13,34 @@ server.use(cookieParser());
 
 server.use('/aaa/a.html',function(req,res){
 //	res.send(req.cookies);
-	
 	req.secret='xuaiooasu65hg66';
 	res.cookie('user1','xiao1',{signed:true});
+	res.send('ok!');
+	res.end();
+})
+
+
+//session
+
+server.use(cookieSession({
+	//手动设置session名
+	name:'sess',
+	//手动设置session密钥
+	keys:['aaa','bbb','ccc'],
+	//手动设置session过期时间，单位为毫秒
+	maxAge:30*60*1000
+}));
+
+server.use('/aaa/b.html',function(req,res){
+	
+	if(req.session['count']==null){
+		req.session['count']=1;
+	}else{
+		req.session['count']++;
+	}
+	
+	//console.log(req.session);
+	console.log(req.session['count']);
 	res.send('ok!');
 	res.end();
 })
